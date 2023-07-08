15675e4f2539627bd85f3751537e360219f73e27
wolfirik <39264658+Serbirial@users.noreply.github.com>
2023-07-08 11:24:10 -0800

    Loading addons fully working.

--- backup/addons.py
+++ backup/addons.py
@@ -1,5 +1,9 @@
-moddable_funcs = set("test_func")
+import importlib.machinery
 
+moddable_funcs = [
+    "compress",
+    "decompress"
+]
 
 from dataclasses import dataclass
 
@@ -9,37 +13,33 @@ class Addon:
     path: str
     funcs: dict
     to_replace: dict
+    insert_imports: callable
 
 def load(paths: list[str]):
     global _addons
     _addons = []
     for file in paths:
         _funcs = {}
-        _raw = __import__(file)
-        if not hasattr(_raw, "export"):
-            print("ADDON HAS NO EXPORT")
+        name = file.split("/")[-1].split(".")[0]
+        _raw = importlib.machinery.SourceFileLoader(name, file).load_module()
+        if not hasattr(_raw, "exports"):
+            print("ADDON HAS NO EXPORTS")
             exit(0)
-        if not all(k in _raw.export for k in ("replaces", "name", "path", "funcs")):
+        if not all(k in _raw.exports for k in ("replaces", "name")):
             print("Export is invalid.")
             exit(0)
-        exports = _raw.export
+        exports = _raw.exports
         if not "replaces" in exports:
             print("Addon export doesnt replace any functions")
             exit(0)
-        for func in exports["funcs"]:
-            if "name" not in func:
-                print("Func in export funcs doesnt have name.")
-                exit(0)
-            _funcs[func["name"]] = func["callable"]  
-        for modified_func_data in exports["replaces"]:
-            if "name" not in modified_func_data:
-                print("Addon is trying to replace a function without specifying/giving a name.")
-                exit(0)
-            elif "name" not in moddable_funcs:
+        for name, _callable in exports["replaces"].items():
+            if name not in moddable_funcs:
                 print("Addon is trying to replace a function that is not replacable.")
                 exit(0)
-            
-        _addons.append(Addon(name=exports["name"], path=exports["path"], funcs=_funcs, to_replace=exports["replaces"]))
+        if not "insert_imports" in dir(_raw):
+            print("Addon is missing function to insert imports")
+            exit(0)
+        _addons.append(Addon(name=exports["name"], path=file, funcs=_funcs, to_replace=exports["replaces"], insert_imports=_raw.insert_imports))
 
 def get_addons():
     return _addons
